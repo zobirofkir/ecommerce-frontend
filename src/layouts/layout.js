0124c6f84a5e7adc3b7db282e364e8f9fe2bb079
zobirofkir <zobirofkir19@gmail.com>
2024-11-26 09:28:11 +0100

    downlad tailwind css

---
+++ src/layouts/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const layout = () => {
+  return (
+    <min>
+        <Outlet/>
+    </min>
+  )
+}
+
+export default layout
