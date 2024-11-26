f63ab83fd2125345da16afe36c29abb71872a9b5
zobirofkir <zobirofkir19@gmail.com>
2024-11-26 14:37:32 +0100

    Last product

---
+++ src/components/LastProduct.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LastProduct = () => {
+  return (
+    <div>LastProduct</div>
+  )
+}
+
+export default LastProduct
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import SliderComponent from '../components/SliderComponent'
+import LastProduct from '../components/LastProduct'
 
 const HomeScreen = () => {
   return (
     <div>
       <SliderComponent/>
+
+      <LastProduct/>
     </div>
   )
 }
