54e272c08d6a3c53d9cd3bb8f56fc6ab5cd3c13f
zobirofkir <zobirofkir19@gmail.com>
2024-11-28 11:35:04 +0100

    create last products

--- src/components/BrandComponent.js
+++ src/components/BrandComponent.js
@@ -10,31 +10,31 @@ const BrandComponent = () => {
               className='bg-cover bg-center px-8 py-3 rounded-md text-white whitespace-nowrap shadow-lg hover:bg-opacity-80 transition-all duration-300 transform hover:scale-105'
               style={{ backgroundImage: 'url(https://ispot.ru/upload/iblock/bc1/saobzuzubgqblxtx7ycb2hmrjrxhomwh/4_1.jpg)' }}
             >
-              Category 1
+              Brand 1
             </li>
             <li
               className='bg-cover bg-center px-8 py-3 rounded-md text-white whitespace-nowrap shadow-lg hover:bg-opacity-80 transition-all duration-300 transform hover:scale-105'
               style={{ backgroundImage: 'url(https://images.samsung.com/n_africa/smartphones/galaxy-s24-ultra/images/galaxy-s24-ultra-share-image.jpg)' }}
             >
-              Category 2
+              Brand 2
             </li>
             <li
               className='bg-cover bg-center px-8 py-3 rounded-md text-white whitespace-nowrap shadow-lg hover:bg-opacity-80 transition-all duration-300 transform hover:scale-105'
               style={{ backgroundImage: 'url(https://www.cdiscount.com/pdt2/8/8/3/1/1920x800/vib5057551887883/rw/vibox-viii-38-pc-gamer-27-165hz-ecran-pack-in.jpg)' }}
             >
-              Category 3
+              Brand 3
             </li>
             <li
               className='bg-cover bg-center px-8 py-3 rounded-md text-white whitespace-nowrap shadow-lg hover:bg-opacity-80 transition-all duration-300 transform hover:scale-105'
               style={{ backgroundImage: 'url(https://ispot.ru/upload/iblock/bc1/saobzuzubgqblxtx7ycb2hmrjrxhomwh/4_1.jpg)' }}
             >
-              Category 4
+              Brand 4
             </li>
             <li
               className='bg-cover bg-center px-8 py-3 rounded-md text-white whitespace-nowrap shadow-lg hover:bg-opacity-80 transition-all duration-300 transform hover:scale-105'
               style={{ backgroundImage: 'url(https://images.samsung.com/n_africa/smartphones/galaxy-s24-ultra/images/galaxy-s24-ultra-share-image.jpg)' }}
             >
-              Category 5
+              Brand 5
             </li>
           </ul>
         </div>
--- src/components/LastProductComponent.js
+++ src/components/LastProductComponent.js
@@ -1,59 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { productAction } from '../redux/actions/ProductAction';
 
 const LastProductsComponent = () => {
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.product);
 
-  const products = [
-    {
-      id: 1,
-      title: 'Product Title 1',
-      description: 'Short description of the product that highlights key features and benefits.',
-      price: 29.99,
-      imageUrl: 'https://images.samsung.com/n_africa/smartphones/galaxy-s24-ultra/images/galaxy-s24-ultra-share-image.jpg',
-      badge: 'New',
-    },
-    {
-      id: 2,
-      title: 'Product Title 2',
-      description: 'Another product description with great features and details.',
-      price: 19.99,
-      imageUrl: 'https://www.cdiscount.com/pdt2/8/8/3/1/1920x800/vib5057551887883/rw/vibox-viii-38-pc-gamer-27-165hz-ecran-pack-in.jpg',
-      badge: 'Sale',
-    },
-    {
-      id: 3,
-      title: 'Product Title 3',
-      description: 'This is another amazing product that you will love.',
-      price: 49.99,
-      imageUrl: 'https://ispot.ru/upload/iblock/bc1/saobzuzubgqblxtx7ycb2hmrjrxhomwh/4_1.jpg',
-      badge: 'New',
-    },
-    {
-      id: 1,
-      title: 'Product Title 1',
-      description: 'Short description of the product that highlights key features and benefits.',
-      price: 29.99,
-      imageUrl: 'https://images.samsung.com/n_africa/smartphones/galaxy-s24-ultra/images/galaxy-s24-ultra-share-image.jpg',
-      badge: 'New',
-    },
-    {
-      id: 2,
-      title: 'Product Title 2',
-      description: 'Another product description with great features and details.',
-      price: 19.99,
-      imageUrl: 'https://www.cdiscount.com/pdt2/8/8/3/1/1920x800/vib5057551887883/rw/vibox-viii-38-pc-gamer-27-165hz-ecran-pack-in.jpg',
-      badge: 'Sale',
-    },
-    {
-      id: 3,
-      title: 'Product Title 3',
-      description: 'This is another amazing product that you will love.',
-      price: 49.99,
-      imageUrl: 'https://ispot.ru/upload/iblock/bc1/saobzuzubgqblxtx7ycb2hmrjrxhomwh/4_1.jpg',
-      badge: 'New',
-    },
+  const [lastTenProducts, setLastTenProducts] = useState([]);
 
-  ]
+  useEffect(() => {
+    dispatch(productAction());
+  }, [dispatch]);
 
+  useEffect(() => {
+    setLastTenProducts(products.slice(-10));
+  }, [products]);
+
+  
   return (
     <div className="container mx-auto p-6">
 
@@ -65,25 +28,25 @@ const LastProductsComponent = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
 
-        {products.map(product => (
+        {lastTenProducts.map(product => (
           <div key={product.id} className="bg-white p-6 rounded-lg shadow-lg border border-gray-200 hover:shadow-xl transform hover:scale-105 transition duration-300">
             <div className="relative">
               
               <a href='/'>
-                <img src={product.imageUrl} alt={product.title} className="w-full h-56 object-cover rounded-md mb-4"/>
+                <img src={product.images.split(',')[0].trim()} alt={product.title} className="w-full h-56 object-cover rounded-md mb-4"/>
               </a>
 
               <span className="absolute top-2 left-2 bg-indigo-600 text-white text-xs font-semibold py-1 px-3 rounded-full">
-                {product.badge}
+                New
               </span>
             </div>
 
             <h3 className="text-xl font-semibold text-gray-800 mb-2">{product.title}</h3>
 
-            <p className="text-sm text-gray-600 mb-4">{product.description}</p>
+            <p className="text-sm text-gray-600 mb-4">{product.description.substring(0, 20)}...</p>
 
             <div className="flex items-center justify-between">
-              <span className="text-xl font-bold text-gray-900">${product.price}</span>
+              <span className="text-xl font-bold text-gray-900">{product.price}</span>
               <div className="flex space-x-4">
                 <button className="text-gray-600 hover:text-gray-800">
                   <i className="fas fa-cart-plus"></i>
